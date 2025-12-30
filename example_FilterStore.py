fa1b1be35d77325d0f24fce87dbd781fef4a4c28
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-12-30 15:13:32 -0300

    Added extra lines between print statements in the example of a FilterStore

--- example_FilterStore.py
+++ example_FilterStore.py
@@ -13,10 +13,14 @@ machine_shop.items = [m1, m2]  # Pre-populate the machine shop
 
 def user(name, env, ms, size):
     machine = yield ms.get(lambda machine: machine.size == size)
-    print(f"{name} got {machine} at {env.now}")
+
+    print(f"\n{name} got {machine} at {env.now}")
+
     yield env.timeout(machine.duration)
+
     yield ms.put(machine)
-    print(f"{name} released {machine} at {env.now}")
+
+    print(f"\n{name} released {machine} at {env.now}")
 
 
 users = [env.process(user(i, env, machine_shop, (i % 2) + 1)) for i in range(3)]
