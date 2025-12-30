2a9bfa3874239d76d0b1bb57b2b44bee47869b96
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-12-30 14:54:25 -0300

    Example using FilterStore from Simpy documentation - https://simpy.readthedocs.io/en/4.0.2/topical_guides/resources.html#stores

---
+++ example_FilterStore.py
@@ -0,0 +1,23 @@
+from collections import namedtuple
+
+import simpy
+
+Machine = namedtuple("Machine", "size, duration")
+m1 = Machine(1, 2)  # Small and slow
+m2 = Machine(2, 1)  # Big and fast
+
+env = simpy.Environment()
+machine_shop = simpy.FilterStore(env, capacity=2)
+machine_shop.items = [m1, m2]  # Pre-populate the machine shop
+
+
+def user(name, env, ms, size):
+    machine = yield ms.get(lambda machine: machine.size == size)
+    print(name, "got", machine, "at", env.now)
+    yield env.timeout(machine.duration)
+    yield ms.put(machine)
+    print(name, "released", machine, "at", env.now)
+
+
+users = [env.process(user(i, env, machine_shop, (i % 2) + 1)) for i in range(3)]
+env.run()
