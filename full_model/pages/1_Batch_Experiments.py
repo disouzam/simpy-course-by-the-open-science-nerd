a056efef04b518d4457b17ebd8c888ead4f7c05c
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-01-04 10:22:33 -0300

    Added widget to enable file upload to upload simulation configurations from CSV files

--- full_model/pages/1_Batch_Experiments.py
+++ full_model/pages/1_Batch_Experiments.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import streamlit as st
 from app_utility.file_io import read_file_contents
 from output_analysis import create_example_csv
@@ -12,3 +13,11 @@ with st.expander("Template to use for experiments"):
     st.markdown(read_file_contents("resources/batch_upload_txt.md"))
     template = create_example_csv()
     st.dataframe(template, hide_index=True)
+
+st.markdown(INFO_2)
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+    df_experiments = pd.read_csv(uploaded_file)
+    st.write("Loaded Experiments")
+    st.table(df_experiments)
