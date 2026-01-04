83ebfaa9ee0a8de22fc13e9a815605a6a3cc3cc3
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-01-04 11:04:55 -0300

    Added an About page

---
+++ full_model/pages/4_About.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Code your first DES model in Python Tutorial")
+st.link_button(
+    "@OpenScienceNerd YouTube", "https://www.youtube.com/@TheOpenScienceNerd"
+)
+
+VIDEO_URL = "https://www.youtube.com/watch?v=FZOnGolWgho"
+st.video(VIDEO_URL)
