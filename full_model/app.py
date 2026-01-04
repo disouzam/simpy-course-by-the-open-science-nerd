7d73985697ee864eb287cd6762add73f81eeecc5
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-01-04 06:52:37 -0300

    Added docstring to the main file of streamlit application and fix a typo

--- full_model/app.py
+++ full_model/app.py
@@ -1,3 +1,9 @@
+"""
+Simpy application using streamlit package
+
+This application implements a discrete event simulation of an urgent care call centre
+"""
+
 import streamlit as st
 
-st.title("A DES model of a Urgent care call centre")
+st.title("A DES model of an Urgent care call centre")
