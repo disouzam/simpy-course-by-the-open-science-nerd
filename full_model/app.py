0665bcf5211cc3d172395f42d591fa71eccb7b59
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-01-04 07:29:06 -0300

    Inserted content from model_info into streamlit app

--- full_model/app.py
+++ full_model/app.py
@@ -5,5 +5,7 @@ This application implements a discrete event simulation of an urgent care call c
 """
 
 import streamlit as st
+from app_utility.file_io import read_file_contents
 
 st.title("A DES model of an Urgent care call centre")
+st.markdown(read_file_contents("./resources/model_info.md"))
